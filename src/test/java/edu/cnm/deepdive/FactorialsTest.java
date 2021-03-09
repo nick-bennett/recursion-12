8204120df3493b993d894ecb8fc1db4f7cf50304
Nick Bennett <nick@nickbenn.com>
2021-03-09 10:37:50 -0700

    Use @CsvFileSource.

--- src/test/java/edu/cnm/deepdive/FactorialsTest.java
+++ src/test/java/edu/cnm/deepdive/FactorialsTest.java
@@ -8,31 +8,23 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class FactorialsTest {
 
   @DisplayName("computeRecursive(non-exceptions)")
   @ParameterizedTest(name = "[{index}] Asserting computeRecursive({0}) == {1}.")
-  @MethodSource
+  @CsvFileSource(resources = "factorials-test-non-exception.csv", numLinesToSkip = 1)
   void computeRecursive_nonException(int input, long expected) {
     assertEquals(expected, Factorials.computeRecursive(input));
   }
 
   @DisplayName("computeRecursive(exceptions)")
-  @Test
-  void computeRecursive_exception() {
-    assertThrows(IllegalArgumentException.class, () -> Factorials.computeRecursive(-1));
-  }
-
-  static List<Arguments> computeRecursive_nonException() {
-    return List.of(
-        Arguments.of(0, 1L),
-        Arguments.of(1, 1L),
-        Arguments.of(5, 120L),
-        Arguments.of(10, 3_628_800L),
-        Arguments.of(13, 6_227_020_800L)
-    );
+  @ParameterizedTest(name = "[{index}] Asserting computeRecursive({0}) throws IllegalArgumentException.")
+  @CsvFileSource(resources = "factorials-test-exception.csv", numLinesToSkip = 1)
+  void computeRecursive_exception(int input) {
+    assertThrows(IllegalArgumentException.class, () -> Factorials.computeRecursive(input));
   }
 
 }
